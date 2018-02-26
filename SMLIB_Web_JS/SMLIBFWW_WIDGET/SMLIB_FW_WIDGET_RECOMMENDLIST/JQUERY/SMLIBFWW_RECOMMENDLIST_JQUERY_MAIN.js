c05638855706293cd289c775e8fc102b49ccda13
raphsutti <neoster9@gmail.com>
2018-02-27 06:41:08 +1100

    initial commit

--- SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
+++ SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
@@ -1,9 +1,66 @@
-﻿(function ($) {
-    // #region reading desk entry point
-    $.fn.smlib_recommendlist = function (options) {
-        var innerhtml = '<div class="alert alert-info"><h3><strong>Code Up Recommend List Widget</strong></h3></div>';
-        $(this).html(innerhtml);
-    }
-    // #endregion
+﻿$(document).ready(function(){
+	var url = "http://vcld.fsdcmel.librarysolutions.com.au/VCLD_FS_TEST/SmartLibrary/Services/Search/SMLIB_SERVICE_SEARCH.asmx/GetRecommendList";
+	$.ajax({
+			type:"GET",
+			url: url,
+			async: false,
+			dataType: "json",
+			success: function(data){
+				// count
+				console.log(data.count);
+				// book1
+				console.log(data.items[0]);
+				// book2
+				console.log(data.items[1]);
+				// Reset table to blank
+				$("#output").html("");
+				// Loop through all objects
+				// for (var i=0; i<data.items.length; i++) {
+				// 	$("#output").prepend("<li><a href= "+data.items[i].title+">"+data.items[i].title+"</a></li>"+
+				// 		"<li>"+data.items[i].author+"</li>"+
+				// 		"<img class=\"cover\" src= "+data.items[i].cover+">"+
+				// 		"<br>"
+				// 		);
+				// }
 
-}(jQuery));
+				for (var i=0; i<data.items.length; i++) {
+					// filter only available items
+					if(parseInt(data.items[i].availablecopy) > 0){
+						// result table
+						$("#output").prepend(
+							"<img class=\"cover\" src= \""+ data.items[i].cover+"\">"+
+							"<table class=\"table table-hover\">"+
+								"<thead>"+
+									"<tr>"+
+									    "<th scope=\"col\" colspan=\"3\">"+data.items[i].title+"</th>"+
+									"</tr>"+
+									"</thead>"+
+									"<tbody>"+
+									"<tr>"+
+									    "<th scope=\"row\">Author</th>"+
+									    "<td>"+data.items[i].author+"</td>"+
+									"</tr>"+
+									"<tr>"+
+									    "<th scope=\"row\">Media</th>"+
+									    "<td>"+data.items[i].media+"</td>"+
+									"</tr>"+
+									"<tr>"+
+									    "<th scope=\"row\">Book ID</th>"+
+									    "<td>"+data.items[i].bookid+"</td>"+
+									"</tr>"+
+									"<tr>"+
+									    "<th scope=\"row\">Available Copy</th>"+
+									    "<td>"+data.items[i].availablecopy+"</td>"+
+									"</tr>"+
+								"</tbody>"+
+							"</table>"+
+							"<br>"
+							);
+					}
+				}
+			},
+			error: function(errorMessage){
+				// alert("Error");
+			}
+		});
+})
