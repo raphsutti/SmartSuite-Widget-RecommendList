d3792d78309946d6d7347415eaf93a62ccfd181a
raphsutti <neoster9@gmail.com>
2018-02-27 19:21:15 +1100

    initial commit

--- SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
+++ SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
@@ -7,11 +7,12 @@
 			dataType: "json",
 			success: function(data){
 				// count
-				console.log(data.count);
-				// book1
-				console.log(data.items[0]);
-				// book2
-				console.log(data.items[1]);
+				// console.log(data.count);
+				// // book1
+				// console.log(data.items[0]);
+				// // book2
+				// console.log(data.items[1]);
+
 				// Reset table to blank
 				$("#output").html("");
 				// Loop through all objects
@@ -22,45 +23,60 @@
 				// 		"<br>"
 				// 		);
 				// }
-
+				// sort data by availablecopy - ascending
+				var sorted = data.items.sort(function (a, b) {
+    				if (a.availablecopy > b.availablecopy) {
+      					return -1;
+      				}
+    				if (a.availablecopy < b.availablecopy) {
+     					 return 1;
+     				}
+    				return 0;
+    			});
+				console.log(data.items);
 				for (var i=0; i<data.items.length; i++) {
+
 					// filter only available items
 					if(parseInt(data.items[i].availablecopy) > 0){
 						// result table
 						$("#output").prepend(
-							"<img class=\"cover\" src= \""+ data.items[i].cover+"\">"+
-							"<table class=\"table table-hover\">"+
-								"<thead>"+
-									"<tr>"+
-									    "<th scope=\"col\" colspan=\"3\">"+data.items[i].title+"</th>"+
-									"</tr>"+
-									"</thead>"+
-									"<tbody>"+
-									"<tr>"+
-									    "<th scope=\"row\">Author</th>"+
-									    "<td>"+data.items[i].author+"</td>"+
-									"</tr>"+
-									"<tr>"+
-									    "<th scope=\"row\">Media</th>"+
-									    "<td>"+data.items[i].media+"</td>"+
-									"</tr>"+
-									"<tr>"+
-									    "<th scope=\"row\">Book ID</th>"+
-									    "<td>"+data.items[i].bookid+"</td>"+
-									"</tr>"+
-									"<tr>"+
-									    "<th scope=\"row\">Available Copy</th>"+
-									    "<td>"+data.items[i].availablecopy+"</td>"+
-									"</tr>"+
-								"</tbody>"+
-							"</table>"+
-							"<br>"
+							"<div class=\"row\">"+
+					            "<div class=\"col-lg-2 col-sm-2\">"+
+					                "<img class=\"cover\" src=\""+data.items[i].cover+"\">"+
+					            "</div>"+
+					            "<div class=\"col-lg-10 col-sm-10\">"+
+									"<table class=\"table table-hover\">"+
+										"<thead>"+
+											"<tr>"+
+											    "<th scope=\"col\" colspan=\"3\">"+data.items[i].title+"</th>"+
+											"</tr>"+
+											"</thead>"+
+											"<tbody>"+
+											"<tr>"+
+											    "<th scope=\"row\">Author</th>"+
+											    "<td>"+data.items[i].author+"</td>"+
+											"</tr>"+
+											"<tr>"+
+											    "<th scope=\"row\">Media</th>"+
+											    "<td>"+data.items[i].media+"</td>"+
+											"</tr>"+
+											"<tr>"+
+											    "<th scope=\"row\">Book ID</th>"+
+											    "<td>"+data.items[i].bookid+"</td>"+
+											"</tr>"+
+											"<tr>"+
+											    "<th scope=\"row\">Available Copy</th>"+
+											    "<td>"+data.items[i].availablecopy+"</td>"+
+											"</tr>"+
+										"</tbody>"+
+									"</table>"+
+								"</div>"
 							);
 					}
 				}
 			},
 			error: function(errorMessage){
-				// alert("Error");
+				alert("Error");
 			}
 		});
 })
