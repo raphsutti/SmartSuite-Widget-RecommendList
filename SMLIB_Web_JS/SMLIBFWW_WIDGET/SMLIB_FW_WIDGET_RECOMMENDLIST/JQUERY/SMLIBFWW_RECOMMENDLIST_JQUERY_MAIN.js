44c65ab20f1f10b7e3da92a4b747e0e35c3da7dd
raphsutti <neoster9@gmail.com>
2018-03-03 12:16:06 +1100

    added cover image zoom hover feature

--- SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
+++ SMLIB_Web_JS/SMLIBFWW_WIDGET/SMLIB_FW_WIDGET_RECOMMENDLIST/JQUERY/SMLIBFWW_RECOMMENDLIST_JQUERY_MAIN.js
@@ -1,48 +1,53 @@
 ﻿$(document).ready(function(){
 	var url = "http://vcld.fsdcmel.librarysolutions.com.au/VCLD_FS_TEST/SmartLibrary/Services/Search/SMLIB_SERVICE_SEARCH.asmx/GetRecommendList";
 	$.ajax({
-			type:"GET",
-			url: url,
-			async: false,
-			dataType: "json",
-			success: function(data){
+		type:"GET",
+		url: url,
+		async: false,
+		dataType: "json",
+		success: function(data){
 
-				// Reset table to blank
-				$("#output").html("");
+			// Reset table to blank
+			$("#output").html("");
 
-				var sorted = data.items.sort(function (a, b) {
-    				if (a.availablecopy > b.availablecopy) {
-      					return -1;
-      				}
-    				if (a.availablecopy < b.availablecopy) {
-     					 return 1;
-     				}
-    				return 0;
-    			});
-				console.log(data.items);
-				var bookNum;
-				for (var i=0; i<data.items.length; i++) {
-					if(parseInt(data.items[i].availablecopy) > 0){
-						bookNum = "red"
-					} else {
-						bookNum = "grey"
-					}
-					// filter only available items
-					// result table
-					$("#output").prepend(
-						
-						"<tr>"+
-				            // "<a href=\"detail.html\" target=\"_blank\""+
-				            "<th scope=\"row\"><img class=\"cover\" src=\""+data.items[i].cover+"\"></th>"+
-				            "<td class='"+ bookNum +"'>"+data.items[i].title+" "+data.items[i].availablecopy+"</td>"+
-				        // "</a>"+ 
-				        "</tr>"
-				      
-						);
+			var sorted = data.items.sort(function (a, b) {
+				if (a.availablecopy > b.availablecopy) {
+  					return -1;
+  				}
+				if (a.availablecopy < b.availablecopy) {
+ 					 return 1;
+ 				}
+				return 0;
+			});
+			// console.log(data.items);
+			
+			var bookNum;
+			for (var i=0; i<data.items.length; i++) {
+				// display grey colour if 0 available copy and red if >0
+				if(parseInt(data.items[i].availablecopy) > 0){
+					bookNum = "red"
+				} else {
+					bookNum = "grey"
 				}
-			},
-			error: function(errorMessage){
-				alert("Error");
+				// result table
+				$("#output").prepend(
+					
+					"<tr class='clickable-row'>"+
+			            "<th scope='row'><img class='cover' src='"+data.items[i].cover+"''></th>"+
+			            "<td class='"+ bookNum +"'><a href='"+ data.items[i].bookid +"' target='_blank'>"+data.items[i].title+" "+data.items[i].availablecopy+"</a></td>"+
+			        "</tr>"
+					);
+				$(".clickable-row").click(function() {
+					console.log($(this).find("a").attr("href"))
+					// window.location = $(this).find("a").attr("href") +".html";
+				});
 			}
-		});
+
+		var lib = data.items
+		console.log(lib)
+		},
+		error: function(errorMessage){
+			alert("Error");
+		}
+	});
 })
